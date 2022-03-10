60246f37f0f2ba6ed3ed12b9044ead5425adab04
Jairo Koning <jairo.koning@gmail.com>
2022-03-10 06:01:02 -0300

    feat: create and style header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div>
-      <h1>DT MONEY</h1>   
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
     
   );
 }
