9c8dce268e85b9b7785723a0d8745e93651fe8ba
Jairo Koning <jairo.koning@gmail.com>
2022-03-13 06:27:25 -0300

    feat: configure miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de Site',
+          amount: 15000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
