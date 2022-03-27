17f98af923125a090818cf1cfc1114daf7d77e20
Jairo Koning <jairo.koning@gmail.com>
2022-03-27 14:23:53 -0300

    fix: create hook use transactions

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,6 +1,6 @@
-import { createContext, useEffect, useState, ReactNode } from "react";
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
 
-import { api } from "./services/api";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -23,7 +23,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -54,4 +54,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+  return context;
 }
